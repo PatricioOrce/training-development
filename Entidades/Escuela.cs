660912ecf1e260f19ac9dce3b99dd46e83ad3abb
Patricio <patodotaop@hotmail.com>
2023-03-24 19:53:19 -0300

    Creacion de nueva entidad solicitada.

---
+++ Entidades/Escuela.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.Entidades
+{
+    public class Escuela
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public List<Persona> Alumnos { get; set; }
+    }
+}
